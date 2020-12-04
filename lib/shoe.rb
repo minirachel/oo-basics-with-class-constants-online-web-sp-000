f7bed86f8d4da5360cc615dfd1845af77ea3ac08
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-12-04 00:12:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,14 +3,14 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
         BRANDS << brand
   end
   # 
   # def brand=(brand)
-  # 
+  #
   # end
 
 
